8041eb90fc4c1122168155fc9f7ba723e3522dc7
Brent Atkinson <brent.atkinson@gmail.com>
2014-04-12 13:08:30 -0400

    Added ability to determine game over and winner.

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -43,26 +43,26 @@ function GameGrid(size, oldgrid) {
          this.rowIndexes[i] = new Array();
          this.colIndexes[i] = new Array();
          for (var j=0; j<this.size; j++) {
-            this.rowIndexes[i].push([ i, j ]);
-            this.colIndexes[i].push([ j, i ]);
+            this.rowIndexes[i].push([i,j]);
+            this.colIndexes[i].push([j,i]);
          }
       }
 
       // Build diag indexes, always only two of grid size
-      this.diagIndexes[0] = new Array();
-      this.diagIndexes[1] = new Array();
+      this.diagIndexes.push(new Array());
+      this.diagIndexes.push(new Array());
       for (var i=0; i<this.size; i++) {
-         this.diagIndexes[0].push([ i, i ]);
-         this.diagIndexes[1].push([ this.size - i, i ]);
+         this.diagIndexes[0].push([i,i]);
+         this.diagIndexes[1].push([this.size-(i+1),i]);
       }
 
       // Construct master of all indexes
       for (var row in this.rowIndexes)
-         this.allIndexes.push(row);
+         this.allIndexes.push(this.rowIndexes[row]);
       for (var col in this.colIndexes)
-         this.allIndexes.push(col);
+         this.allIndexes.push(this.colIndexes[col]);
       for (var diag in this.diagIndexes)
-         this.allIndexes.push(diag);
+         this.allIndexes.push(this.diagIndexes[diag]);
    };
 
    this.getMark = function(row, col) {
@@ -70,21 +70,54 @@ function GameGrid(size, oldgrid) {
    };
 
    this.setMark = function(row, col, player) {
-     if (typeof this.getMark(row,col) !== "undefined" && typeof player !== "undefined") {
-       throw "already marked";
-     }
-     this.grid[row][col] = player;
+      var playerDefined = typeof player !== "undefined";
+      var alreadyMarked = typeof this.getMark(row,col) !== "undefined";
+      if (alreadyMarked && playerDefined) {
+         throw "cell already marked";
+      }
+      this.grid[row][col] = player;
+      if (playerDefined) {
+         this.moves++;
+      }
+      else if (alreadyMarked) {
+         this.moves--;
+      }
    };
 
+   this.getWinner = function() {
+      var playerWins = function (cell) { return this.getMark(cell[0],cell[1]) === PLAYER; };
+      var computerWins = function (cell) { return this.getMark(cell[0],cell[1]) === COMPUTER; };
+      for (lineIdx in this.allIndexes) {
+         var line = this.allIndexes[lineIdx];
+         if (line.every(playerWins,this)) {
+            return PLAYER;
+         }
+         if (line.every(computerWins,this)) {
+            return COMPUTER;
+         }
+      }
+      return undefined;
+   };
+
+   this.isGameComplete = function() {
+      if (this.moves >= this.moveMax) {
+         return true;
+      }
+      var winner = this.getWinner();
+      if (typeof winner !== "undefined") {
+         return true;
+      }
+      return false;
+   };
    
    if (typeof oldgrid === "undefined") {
       this.size = size;
-      this.move_max = size * size;
+      this.moveMax = size * size;
       this.moves = 0;
       this.initGrid();
    } else {
       this.size = oldgrid.size;
-      this.move_max = oldgrid.move_max;
+      this.moveMax = oldgrid.moveMax;
       this.moves = oldgrid.moves;
       this.grid = this.copyGrid(oldgrid.grid);
    }
@@ -128,7 +161,12 @@ function TicTacToe(formName, gridSize) {
    this.updateButtons = function() {
       for (var row=0; row<this.grid.size; row++) {
          for (var col=0; col<this.grid.size; col++) {
-            this.button(row,col).innerHTML = this.gridLabel(row,col);
+            var gridLabel = this.gridLabel(row,col);
+            var button = this.button(row,col);
+            button.innerHTML = gridLabel;
+            if (gridLabel !== ' ')  {
+               button.disabled = true;
+            }
          }
       }
    };
@@ -136,6 +174,11 @@ function TicTacToe(formName, gridSize) {
    this.initialize = function() {
       this.createButtons();
       this.grid.setMark(0,0,PLAYER);
+      this.grid.setMark(0,1,PLAYER);
+      this.grid.setMark(0,2,PLAYER);
       this.updateButtons();
+      var gameover = this.grid.isGameComplete();
+      var winner = this.grid.getWinner();
+      alert('game over? ' + gameover + ' ' + (typeof winner !== 'undefined'? winner.label + ' wins!' : ''));
    };
 }
